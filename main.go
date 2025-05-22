cfe934d20eca668130132739a1caabad7fee7d57
raihan <raihanfg565@example.com>
2025-05-22 08:43:04 +0800

    menambahkan cors

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 	route "go-apotik-api/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
-	
+
+	// Tambahkan middleware CORS di sini
+	app.Use(cors.New(cors.Config{
+	AllowOrigins:     "http://localhost:5174",
+	AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
+	AllowHeaders:     "*", // atau bisa sebutkan secara spesifik
+	AllowCredentials: true,
+}))
+
 
 	database.Connect()
 
--- models/user.go
+++ models/user.go
@@ -3,8 +3,8 @@ package models
 type User struct {
 	IDUser   string `json:"id_user" db:"id_user"`
 	Email    string `json:"email" db:"email"`
-	Password string `json:"password" db:"password"` // Simpan hashed password
+	Password string `json:"password" db:"password"` 
 	NamaUser string `json:"nama_user" db:"nama_user"`
-	Role     string `json:"role" db:"role"`           // "admin" atau "petugas"
-	FotoUser string `json:"foto_user" db:"foto_user"` // nama file foto user
+	Role     string `json:"role" db:"role"`           
+	FotoUser string `json:"foto_user" db:"foto_user"` 
 }
